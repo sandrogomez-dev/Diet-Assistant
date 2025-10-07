723fc9e124a6c387870f4a4031cf67be72ab272a
SandroDev <sandrodevx@gmail.com>
2025-10-07 22:01:05 +0200

    back-end ready

--- Diet-Assistant/app.js
+++ Diet-Assistant/app.js
@@ -21,6 +21,51 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+// Metodo para hacer peticion a la IA
+const generateDiet = async (userResponses) => {
+  // crrear el prompt(sistema, indicaciones para la ia.)
+  const promptSystem = {
+    role: "system",
+    content: `Eres un asistente nutricional que ayuda a crear dietas personalizadas.
+    El usuario te dara su peso, altura, objetivo, alergias, alimentos que no le gustan y numero de comidas diarias.
+    Con esta informacion, crea una dieta semanal equilibrada y saludable.
+    el sistema no respondera a ningun otro tipo de preguntas que no sean relacionadas con la nutricion y dietas.`,
+  };
+  // crear el prompt del usuario
+  const promptUser = {
+    role: "user",
+    content: `Hola, quiero que me ayudes a crear una dieta personalizada para una persona que pesa ${userResponses.peso} 
+    kg, mide ${userResponses.altura}
+     cm, tiene como objetivo ${userResponses.objetivo},
+     tiene las siguientes alergias o restricciones alimentarias: ${userResponses.alergias},
+      no le gustan los siguientes alimentos: ${userResponses.no_gusta},
+        y quiere hacer ${userResponses.comidas_diarias} comidas diarias.
+        Devuelve la dieta en formato tabla markdown con las siguiente columnas:
+        Dia, Comida, Ingredientes, Cantidades, Instrucciones.
+        y no digas nada mas, solo devuelve la tabla.
+        `,
+  };
+
+  // hacer peticion a LLM de openai
+  try {
+    const completion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [promptSystem, promptUser],
+      max_tokens: 1000,
+      temperature: 0.7,
+    });
+    // devolvemos el resultado generado
+    const response = completion.choices[0].message.content.trim();
+    return response;
+    // devolver la respuesta generada por la ia
+    return res.json({ reply: completion.choices[0].message.content });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ reply: "Error al generar la dieta" });
+  }
+};
+
+// Objeto de almacenamiento temporal de respuestas del usuario
 let userDAta = {};
 //ruta /  endpoint / url
 app.post("/api/nutri-chat", async (req, res) => {
@@ -74,14 +119,12 @@ app.post("/api/nutri-chat", async (req, res) => {
     userData[userId].comidas_diarias = userMessage;
 
     // Ejecutar peticion a la ia con un prompt
+    const diet = await generateDiet(userData(userId));
 
     // recoger la respuesta y darle la dieta al usuario
 
     // devovler respuesta al usuario
-
-    return res.json({
-      reply: "Gracias por la informacion, generando tu plan de dieta...",
-    });
+    return res.json({ reply: `Aqui tienes tu dieta: ${diet}` });
   }
 
   if (
